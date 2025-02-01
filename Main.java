ea0bd8fb4061951b8b3e838dc35d022ceb0a368f
Akshay <akshaypandey.study@gmail.com>
2025-02-01 14:17:36 +0530

    hello

--- Main.java
+++ Main.java
@@ -15,9 +15,11 @@ public class Main {
 
         int max = Math.max(num1, Math.max(num2, num3));
         int sum = num1 + num2 + num3;
+        double average = sum / 3.0;
 
         System.out.println("The largest number is: " + max);
-        System.out.println("The sum of the largest number is: " + sum);
+        System.out.println("The sum of the numbers is: " + sum);
+        System.out.println("The average of the numbers is: " + average);
 
         scanner.close();
     }
